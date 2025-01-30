31c93ebae41654f969ba8a235dac4ff321077abb
MochiLookingCat <Seebrandonnow@gmail.com>
2025-01-30 16:11:12 -0500

    Got rid of syntax error from MecanumDrive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/DriveConstants.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+public class DriveConstants {
+    public static final double TICKS_PER_REV = 537.6; // Rev HD Hex Motor
+    public static final double MAX_RPM = 312;
+    public static final double WHEEL_RADIUS = 2; // inches
+    public static final double TRACK_WIDTH = 12; // inches
+    public static final double MAX_VEL = 30; // inches per second
+    public static final double MAX_ACCEL = 30;
+    public static final double MAX_ANG_VEL = Math.toRadians(180);
+    public static final double MAX_ANG_ACCEL = Math.toRadians(180);
+
+}
