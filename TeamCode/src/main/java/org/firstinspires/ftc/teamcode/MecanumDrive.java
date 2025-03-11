3f6961f793fbf19fae7f5a9ec02854d4ee4599d6
Eric Zhang <ericbkzhang@gmail.com>
2025-03-11 11:52:18 -0400

    Save tuning results

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -63,13 +63,13 @@ public final class MecanumDrive {
 
         // drive model parameters
         public double inPerTick = 0.0096463022508039;
-        public double lateralInPerTick = inPerTick;
+        public double lateralInPerTick = 0.0167539267;
         public double trackWidthTicks = 0;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
-        public double kA = 0;
+        public double kS = 0.0618814297544672;
+        public double kV = 0.004867394803822;
+        public double kA = 0.0006;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
